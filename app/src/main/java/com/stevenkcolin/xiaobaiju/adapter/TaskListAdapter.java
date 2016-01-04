b7eae030680ef075ac36300901403b2b76a634b2
chenlin <chenlin@gepingtech.com>
2016-01-04 11:00:08 +0800

    添加代码点checkbox，实现文字划线。

--- app/src/main/java/com/stevenkcolin/xiaobaiju/adapter/TaskListAdapter.java
+++ app/src/main/java/com/stevenkcolin/xiaobaiju/adapter/TaskListAdapter.java
@@ -1,14 +1,23 @@
 package com.stevenkcolin.xiaobaiju.adapter;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.v7.widget.AppCompatCheckBox;
+import android.text.Editable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import com.stevenkcolin.xiaobaiju.R;
+import com.stevenkcolin.xiaobaiju.activity.BaseActivity;
+import com.stevenkcolin.xiaobaiju.activity.TaskDetailActivity;
+import com.stevenkcolin.xiaobaiju.activity.TaskListActivity;
 import com.stevenkcolin.xiaobaiju.util.DateUtil;
 import com.stevenkcolin.xiaobaiju.vo.Task;
 
@@ -19,6 +28,7 @@ import java.util.List;
  */
 public class TaskListAdapter extends ArrayAdapter<Task> {
     private int resourceId;
+    public String taskDesc;
 
     public TaskListAdapter(Context context, int textViewResourceId, List<Task> objects) {
         super(context, textViewResourceId, objects);
@@ -27,14 +37,35 @@ public class TaskListAdapter extends ArrayAdapter<Task> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        Task task = getItem(position);
-        View view;
-        ViewHolder  viewHolder;
+        final Task task = getItem(position);
+        final View view;
+        final ViewHolder  viewHolder;
         if (convertView == null) {
             view = LayoutInflater.from(getContext()).inflate(resourceId, null);
             viewHolder = new ViewHolder();
             viewHolder.checkCompleted = (AppCompatCheckBox)view.findViewById(R.id.task_completed);
-            viewHolder.textDueDate = (TextView)view.findViewById(R.id.task_due_date);
+
+            viewHolder.checkCompleted.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    boolean status = viewHolder.checkCompleted.isChecked();
+                    if (status==true) {
+                        viewHolder.checkCompleted.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG);
+                    }else
+                    {
+                        viewHolder.checkCompleted.setPaintFlags(0);
+                    }
+                }
+            });
+
+            viewHolder.checkCompleted.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    // TODO: 1/4/16 添加长按编辑功能
+                    return true;
+                }
+            });
+            viewHolder.textDueDate = (TextView) view.findViewById(R.id.task_due_date);
             view.setTag(viewHolder);
         } else {
             view = convertView;
@@ -43,7 +74,6 @@ public class TaskListAdapter extends ArrayAdapter<Task> {
         viewHolder.checkCompleted.setText(task.getTitle());
         viewHolder.checkCompleted.setChecked(task.isCompleted());
         if (task.getDueDate() != null) {
-
             viewHolder.textDueDate.setText(DateUtil.toLocalStringDateMonth(task.getDueDate()));
         }
         return view;
