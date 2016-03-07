ab2577b8e8fd7d46b06d507077151037e7ae6363
chenlin <chenlin@gepingtech.com>
2016-03-07 13:29:49 +0800

    新登录页面

--- app/src/main/java/com/stevenkcolin/xiaobaiju/activity/NewLoginActivity.java
+++ app/src/main/java/com/stevenkcolin/xiaobaiju/activity/NewLoginActivity.java
@@ -1,18 +1,31 @@
 package com.stevenkcolin.xiaobaiju.activity;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.stevenkcolin.xiaobaiju.R;
+import com.stevenkcolin.xiaobaiju.constant.GeneralConstant;
+import com.stevenkcolin.xiaobaiju.service.UserService;
+import com.stevenkcolin.xiaobaiju.util.DialogUtil;
 import com.umeng.socialize.PlatformConfig;
+import com.umeng.socialize.UMAuthListener;
 import com.umeng.socialize.UMShareAPI;
 import com.umeng.socialize.bean.SHARE_MEDIA;
 
+import java.util.Map;
+
 public class NewLoginActivity extends BaseActivity {
     private UMShareAPI mShareAPI = null;
+    private ProgressDialog mProgressDialog;
+
     private Button mBtnQQLogin;
     private Button mBtnWeixinLogin;
     private Button mBtnWeibo;
@@ -81,112 +94,93 @@ public class NewLoginActivity extends BaseActivity {
 
     //添加登录QQ的代码
     public void loginQQ(View view) {
-        if (view.getId() == R.id.login_account) {
+        if (view.getId() == R.id.btn_QQ_Login) {
             SHARE_MEDIA platform = SHARE_MEDIA.QQ;
             /**begin invoke umeng api**/
             try {
-//                mShareAPI.doOauthVerify(this, platform, umAuthListener);
-
+                mShareAPI.doOauthVerify(this, platform, umAuthListener);
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
         }
     }
 
-//    /**
-//     * auth callback interface
-//     **/
-//    private UMAuthListener umAuthListener = new UMAuthListener() {
-//        @Override
-//        public void onComplete(SHARE_MEDIA platform, int action, Map<String, String> data) {
-//            if (data != null && data.get("openid") != null && data.get("screen_name") == null) {
-//
-//                mShareAPI.getPlatformInfo(this, platform, umAuthListener);
-//            } else if (data != null && data.get("openid") != null && data.get("screen_name") != null) {
-//                String openId = data.get("openid");
-//                String name = data.get("screen_name");
-//                String imageUrl = data.get("profile_image_url");
-//                new LoginTask().execute(new String[]{GeneralConstant.QQ, openId, name, imageUrl});
-//            }
-//
-//        }
-//
-//        @Override
-//        public void onError(SHARE_MEDIA platform, int action, Throwable t) {
-//            Toast.makeText(, "Authorize fail", Toast.LENGTH_SHORT).show();
-//        }
-//
-//        @Override
-//        public void onCancel(SHARE_MEDIA platform, int action) {
-//            Toast.makeText(getContext(), "Authorize cancel", Toast.LENGTH_SHORT).show();
-//        }
-//    };
-//
-//    @Override
-//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//        mShareAPI.onActivityResult(requestCode, resultCode, data);
-//    }
-//
-//    //登录
-//    class LoginTask extends AsyncTask<String, Integer, Boolean> {
-//        @Override
-//        protected void onPreExecute() {
-//            mProgressDialog = DialogUtil.showWaitDialog(this, getString(R.string.please_wait));
-//        }
-//
-//        @Override
-//        protected Boolean doInBackground(String... params) {
-//            String loginFrom = params[0];
-//            String loginAccount = params[1];
-//            String name = params[2];
-//            try {
-//                UserService userService = new UserService();
-//                userService.login3rdAcountExist(loginFrom, loginAccount, name, getActivity());
-//                return true;
-//            } catch (Exception e) {
-//                return false;
-//            }
-//        }
-//
-//        @Override
-//        protected void onPostExecute(Boolean aBoolean) {
-//            if (aBoolean) {
-//                txtName.setText(User.getUser().getName());
-//                btnLogin.setVisibility(View.GONE);
-//                btnLogout.setVisibility(View.VISIBLE);
-//                if (!SyncUtil.isSync) {
-//                    new SyncTask().execute();
-//                }
-//                mProgressDialog.dismiss();
-//            } else {
-//                mProgressDialog.dismiss();
-//                Toast.makeText(getActivity(), getString(R.string.error_network), Toast.LENGTH_SHORT).show();
-//            }
-//        }
-//    }
-//
-//    class SyncTask extends AsyncTask<String, Integer, Boolean> {
-//        @Override
-//        protected void onPreExecute() {
-//            SyncUtil.isSync = true;
-//        }
-//
-//        @Override
-//        protected Boolean doInBackground(String... params) {
-//            TaskService taskService = new TaskService();
-//            try {
-//                taskService.syncTask();
-//                return true;
-//            } catch (Exception e) {
-//                return false;
-//            }
-//        }
-//
-//        @Override
-//        protected void onPostExecute(Boolean aBoolean) {
-//            SyncUtil.isSync = false;
-//            mProgressDialog.dismiss();
-//        }
-//    }
+    /**
+     * auth callback interface
+     **/
+    private UMAuthListener umAuthListener = new UMAuthListener() {
+        @Override
+        public void onComplete(SHARE_MEDIA platform, int action, Map<String, String> data) {
+            if (data != null && data.get("openid") != null && data.get("screen_name") == null) {
+                mShareAPI.getPlatformInfo(NewLoginActivity.this, platform, umAuthListener);
+            } else if (data != null && data.get("openid") != null && data.get("screen_name") != null) {
+                String openId = data.get("openid");
+                String name = data.get("screen_name");
+                String imageUrl = data.get("profile_image_url");
+                new LoginTask().execute(new String[]{GeneralConstant.QQ, openId, name, imageUrl});
+            }
+
+        }
+
+        @Override
+        public void onError(SHARE_MEDIA platform, int action, Throwable t) {
+            Toast.makeText(NewLoginActivity.this, "Authorize fail", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onCancel(SHARE_MEDIA platform, int action) {
+            Toast.makeText(NewLoginActivity.this, "Authorize cancel", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mShareAPI.onActivityResult(requestCode, resultCode, data);
+    }
+
+    //登录
+    class LoginTask extends AsyncTask<String, Integer, Boolean> {
+        @Override
+        protected void onPreExecute() {
+            mProgressDialog = DialogUtil.showWaitDialog(NewLoginActivity.this, getString(R.string.please_wait));
+        }
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            try {
+                String loginFrom = params[0];
+                String loginAccount = params[1];
+                String name = params[2];
+                try {
+                    UserService userService = new UserService();
+                    userService.login3rdAcountExist(loginFrom, loginAccount, name, NewLoginActivity.this);
+                    return true;
+                } catch (Exception e) {
+                    return false;
+                }
+            }
+            catch (Exception e) {
+                return false;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            if (aBoolean) {
+                mProgressDialog.dismiss();
+            } else {
+                mProgressDialog.dismiss();
+                Toast.makeText(NewLoginActivity.this, getString(R.string.error_network), Toast.LENGTH_SHORT).show();
+            }
+            Intent i = new Intent();
+            setResult(Activity.RESULT_OK,i);
+            finish();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
 }
--- app/src/main/java/com/stevenkcolin/xiaobaiju/fragment/MineFragment.java
+++ app/src/main/java/com/stevenkcolin/xiaobaiju/fragment/MineFragment.java
@@ -1,8 +1,8 @@
 package com.stevenkcolin.xiaobaiju.fragment;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -14,20 +14,11 @@ import android.widget.Toast;
 
 import com.stevenkcolin.xiaobaiju.R;
 import com.stevenkcolin.xiaobaiju.activity.NewLoginActivity;
-import com.stevenkcolin.xiaobaiju.constant.GeneralConstant;
 import com.stevenkcolin.xiaobaiju.constant.ReportConstant;
 import com.stevenkcolin.xiaobaiju.model.User;
 import com.stevenkcolin.xiaobaiju.report.ActionInfo;
-import com.stevenkcolin.xiaobaiju.service.TaskService;
-import com.stevenkcolin.xiaobaiju.service.UserService;
-import com.stevenkcolin.xiaobaiju.util.DialogUtil;
-import com.stevenkcolin.xiaobaiju.util.SyncUtil;
 import com.umeng.socialize.PlatformConfig;
-import com.umeng.socialize.UMAuthListener;
 import com.umeng.socialize.UMShareAPI;
-import com.umeng.socialize.bean.SHARE_MEDIA;
-
-import java.util.Map;
 
 /**
  * Created by Pengfei on 2016/2/2.
@@ -36,12 +27,12 @@ public class MineFragment extends BaseFragment {
     private UMShareAPI mShareAPI = null;
 
     private ProgressDialog mProgressDialog;
-    private TextView txtName;
-    private Button btnLogin;
-    private Button btnLogout;
+    private TextView mTxtName;
+    private Button mBtnLogin;
+    private Button mBtnLogout;
+    private Button mBtnAboutUs;
 
-    private NewLoginFragment mFragmentLogin = new NewLoginFragment();
-    private BaseFragment mFragment;
+    private final int FIRST_REQUEST_CODE = 1;
 
     @Nullable
     @Override
@@ -53,41 +44,45 @@ public class MineFragment extends BaseFragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        txtName = (TextView)getView().findViewById(R.id.user_name);
-        btnLogin = (Button)getView().findViewById(R.id.login_account);
-        btnLogout = (Button)getView().findViewById(R.id.logout_account);
+        mTxtName = (TextView)getView().findViewById(R.id.user_name);
+        mBtnLogin = (Button)getView().findViewById(R.id.login_account);
+        mBtnLogout = (Button)getView().findViewById(R.id.logout_account);
+        mBtnAboutUs = (Button)getView().findViewById(R.id.about_us);
 
+        //设置几个按钮的onclick事件
+        initMineButtons();
+        //设置登录状态
+        setLoginStatus();
+        //初始化登录状态
+        initLogin();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        setLoginStatus();
+        fresh();
+    }
+
+    private void setLoginStatus()
+    {
         if (User.getUser().getId() == null) {
-            btnLogin.setVisibility(View.VISIBLE);
-            btnLogout.setVisibility(View.GONE);
+            mBtnLogin.setVisibility(View.VISIBLE);
+            mBtnLogout.setVisibility(View.GONE);
         } else {
-            btnLogin.setVisibility(View.GONE);
-            btnLogout.setVisibility(View.VISIBLE);
+            mBtnLogin.setVisibility(View.GONE);
+            mBtnLogout.setVisibility(View.VISIBLE);
         }
 
         if (User.getUser().getName() != null) {
-            txtName.setText(User.getUser().getName());
+            mTxtName.setText(null);
+            mTxtName.append(User.getUser().getName() + "\n");
+//            mTxtName.append(User.getUser().getId()+"\n");
+//            mTxtName.append(User.getUser().getLoginAccount()+"\n");
+//            mTxtName.append(User.getUser().getLoginFrom()+"\n");
         }
-
-        //添加aboutUs事件
-        openAboutUs();
-        initLogin();
-        openLoginPage();
     }
 
-    //给左侧餐单按钮about_us添加事件，欢迎打开官方网站
-    private void openAboutUs() {
-        getView().findViewById(R.id.about_us).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getContext(), R.string.txt_about_us_cotent, Toast.LENGTH_SHORT).show();
-                //添加打点上报代码
-                ActionInfo mActionInfo = new ActionInfo(ReportConstant.REPORT_MENU_ABOUTUS);
-                mReport.saveOnClick(getContext(), mActionInfo);
-                // TODO: 1/8/16 完成官方网站的web端，并打开官方网站。
-            }
-        });
-    }
 
     private void initLogin() {
         /** init auth api**/
@@ -116,149 +111,172 @@ public class MineFragment extends BaseFragment {
     }
 
     //给左侧菜单按钮login_account添加事件，打开login Page
-    private void openLoginPage() {
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+    private void initMineButtons() {
+        mBtnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startNewLoginPage();
             }
         });
-        btnLogout.setOnClickListener(new View.OnClickListener() {
+        mBtnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                return;
+                logout();
+            }
+        });
+        mBtnAboutUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                aboutUs();
             }
         });
     }
-
-    private void startNewLoginFragment() {
-//        Intent intent = new Intent(getActivity(),NewLoginFragment.class);
-//        intent.setAction("Login");
-//        startActivity(intent);
-        mFragment = mFragmentLogin;
-        //this.getActivity().getSupportFragmentManager()
-        getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.login_account, mFragment).commit();
-
+    //打开AboutUs WebSite
+    private void aboutUs(){
+        Toast.makeText(getContext(), R.string.txt_about_us_cotent, Toast.LENGTH_SHORT).show();
+        //添加打点上报代码
+        ActionInfo mActionInfo = new ActionInfo(ReportConstant.REPORT_MENU_ABOUTUS);
+        mReport.saveOnClick(getContext(), mActionInfo);
+        // TODO: 1/8/16 完成官方网站的web端，并打开官方网站。
+    }
+    //登出
+    private void logout() {
+        User.getUser().setName(null);
+        User.getUser().setId(null);
+        User.getUser().setLoginAccount(null);
+        User.getUser().setLoginAccount(null);
+        mTxtName.setText(null);
+        mBtnLogin.setVisibility(View.VISIBLE);
+        mBtnLogout.setVisibility(View.GONE);
     }
 
+    //打开登录页面，包含QQ登录，微信登录，微博登录
     private void startNewLoginPage() {
         Intent intent = new Intent(getActivity(), NewLoginActivity.class);
         intent.setAction("Login");
-        startActivity(intent);
+        startActivityForResult(intent, FIRST_REQUEST_CODE);
     }
-
-    //添加登录QQ的代码
-    public void loginQQ(View view) {
-        if (view.getId() == R.id.login_account) {
-            SHARE_MEDIA platform = SHARE_MEDIA.QQ;
-            /**begin invoke umeng api**/
-            try {
-                mShareAPI.doOauthVerify(getActivity(), platform, umAuthListener);
-
-                //添加打点上报代码
-                ActionInfo mActionInfo = new ActionInfo(ReportConstant.REPORT_MENU_LOGIN);
-                mReport.saveOnClick(getContext(), mActionInfo);
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        }
-    }
-
-    /**
-     * auth callback interface
-     **/
-    private UMAuthListener umAuthListener = new UMAuthListener() {
-        @Override
-        public void onComplete(SHARE_MEDIA platform, int action, Map<String, String> data) {
-            if (data != null && data.get("openid") != null && data.get("screen_name") == null) {
-                mShareAPI.getPlatformInfo(getActivity(), platform, umAuthListener);
-            } else if (data != null && data.get("openid") != null && data.get("screen_name") != null) {
-                String openId = data.get("openid");
-                String name = data.get("screen_name");
-                String imageUrl = data.get("profile_image_url");
-                new LoginTask().execute(new String[]{GeneralConstant.QQ, openId, name, imageUrl});
-            }
-
-        }
-
-        @Override
-        public void onError(SHARE_MEDIA platform, int action, Throwable t) {
-            Toast.makeText(getContext(), "Authorize fail", Toast.LENGTH_SHORT).show();
-        }
-
-        @Override
-        public void onCancel(SHARE_MEDIA platform, int action) {
-            Toast.makeText(getContext(), "Authorize cancel", Toast.LENGTH_SHORT).show();
-        }
-    };
-
-    @Override
+    //处理NewLoginActivity的页面返回值
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        mShareAPI.onActivityResult(requestCode, resultCode, data);
-    }
-
-    //登录
-    class LoginTask extends AsyncTask<String, Integer, Boolean> {
-        @Override
-        protected void onPreExecute() {
-            mProgressDialog = DialogUtil.showWaitDialog(getActivity(), getString(R.string.please_wait));
-        }
-
-        @Override
-        protected Boolean doInBackground(String... params) {
-            String loginFrom = params[0];
-            String loginAccount = params[1];
-            String name = params[2];
-            try {
-                UserService userService = new UserService();
-                userService.login3rdAcountExist(loginFrom, loginAccount, name, getActivity());
-                return true;
-            } catch (Exception e) {
-                return false;
-            }
-        }
-
-        @Override
-        protected void onPostExecute(Boolean aBoolean) {
-            if (aBoolean) {
-                txtName.setText(User.getUser().getName());
-                btnLogin.setVisibility(View.GONE);
-                btnLogout.setVisibility(View.VISIBLE);
-                if (!SyncUtil.isSync) {
-                    new SyncTask().execute();
-                }
-                mProgressDialog.dismiss();
-            } else {
-                mProgressDialog.dismiss();
-                Toast.makeText(getActivity(), getString(R.string.error_network), Toast.LENGTH_SHORT).show();
-            }
+        if(resultCode== Activity.RESULT_OK){
+            //// TODO: 3/4/16
         }
     }
 
-    class SyncTask extends AsyncTask<String, Integer, Boolean> {
-        @Override
-        protected void onPreExecute() {
-            SyncUtil.isSync = true;
-        }
 
-        @Override
-        protected Boolean doInBackground(String... params) {
-            TaskService taskService = new TaskService();
-            try {
-                taskService.syncTask();
-                return true;
-            } catch (Exception e) {
-                return false;
-            }
-        }
 
-        @Override
-        protected void onPostExecute(Boolean aBoolean) {
-            SyncUtil.isSync = false;
-            mProgressDialog.dismiss();
-        }
-    }
+//    //添加登录QQ的代码
+//    public void loginQQ(View view) {
+//        if (view.getId() == R.id.login_account) {
+//            SHARE_MEDIA platform = SHARE_MEDIA.QQ;
+//            /**begin invoke umeng api**/
+//            try {
+//                mShareAPI.doOauthVerify(getActivity(), platform, umAuthListener);
+//                //添加打点上报代码
+//                ActionInfo mActionInfo = new ActionInfo(ReportConstant.REPORT_MENU_LOGIN);
+//                mReport.saveOnClick(getContext(), mActionInfo);
+//            } catch (Exception ex) {
+//                ex.printStackTrace();
+//            }
+//        }
+//    }
+//
+//    /**
+//     * auth callback interface
+//     **/
+//    private UMAuthListener umAuthListener = new UMAuthListener() {
+//        @Override
+//        public void onComplete(SHARE_MEDIA platform, int action, Map<String, String> data) {
+//            if (data != null && data.get("openid") != null && data.get("screen_name") == null) {
+//                mShareAPI.getPlatformInfo(getActivity(), platform, umAuthListener);
+//            } else if (data != null && data.get("openid") != null && data.get("screen_name") != null) {
+//                String openId = data.get("openid");
+//                String name = data.get("screen_name");
+//                String imageUrl = data.get("profile_image_url");
+//                new LoginTask().execute(new String[]{GeneralConstant.QQ, openId, name, imageUrl});
+//            }
+//
+//        }
+//
+//        @Override
+//        public void onError(SHARE_MEDIA platform, int action, Throwable t) {
+//            Toast.makeText(getContext(), "Authorize fail", Toast.LENGTH_SHORT).show();
+//        }
+//
+//        @Override
+//        public void onCancel(SHARE_MEDIA platform, int action) {
+//            Toast.makeText(getContext(), "Authorize cancel", Toast.LENGTH_SHORT).show();
+//        }
+//    };
+//
+    //    @Override
+    //    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    //        super.onActivityResult(requestCode, resultCode, data);
+    //        mShareAPI.onActivityResult(requestCode, resultCode, data);
+    //    }
+//
+//    //登录
+//    class LoginTask extends AsyncTask<String, Integer, Boolean> {
+//        @Override
+//        protected void onPreExecute() {
+//            mProgressDialog = DialogUtil.showWaitDialog(getActivity(), getString(R.string.please_wait));
+//        }
+//
+//        @Override
+//        protected Boolean doInBackground(String... params) {
+//            String loginFrom = params[0];
+//            String loginAccount = params[1];
+//            String name = params[2];
+//            try {
+//                UserService userService = new UserService();
+//                userService.login3rdAcountExist(loginFrom, loginAccount, name, getActivity());
+//                return true;
+//            } catch (Exception e) {
+//                return false;
+//            }
+//        }
+//
+//        @Override
+//        protected void onPostExecute(Boolean aBoolean) {
+//            if (aBoolean) {
+//                mTxtName.setText(User.getUser().getName());
+//                mBtnLogin.setVisibility(View.GONE);
+//                mBtnLogout.setVisibility(View.VISIBLE);
+//                if (!SyncUtil.isSync) {
+//                    new SyncTask().execute();
+//                }
+//                mProgressDialog.dismiss();
+//            } else {
+//                mProgressDialog.dismiss();
+//                Toast.makeText(getActivity(), getString(R.string.error_network), Toast.LENGTH_SHORT).show();
+//            }
+//        }
+//    }
+//
+//    class SyncTask extends AsyncTask<String, Integer, Boolean> {
+//        @Override
+//        protected void onPreExecute() {
+//            SyncUtil.isSync = true;
+//        }
+//
+//        @Override
+//        protected Boolean doInBackground(String... params) {
+//            TaskService taskService = new TaskService();
+//            try {
+//                taskService.syncTask();
+//                return true;
+//            } catch (Exception e) {
+//                return false;
+//            }
+//        }
+//
+//        @Override
+//        protected void onPostExecute(Boolean aBoolean) {
+//            SyncUtil.isSync = false;
+//            mProgressDialog.dismiss();
+//        }
+//    }
 
 
 }
